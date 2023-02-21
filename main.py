6938f5c0be2891ff233c791978df83f2ee51dda8
AnaHeline <anaheline@gmail.com>
2023-02-21 18:52:23 -0300

    maior e menor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Bem bindo no jogo de adivinhação")
+print("Bem vindo no jogo de adivinhação")
 print("******************************")
 
 numero_secreto=42
@@ -7,10 +7,17 @@ chute_str = input("Digite o seu numero: ")
 
 print("Você digitou", chute_str)
 chute = int(chute_str)
-42
-if (numero_secreto == chute):
+
+acertou     = numero_secreto == chute
+errou_maior = chute > numero_secreto
+errou_menor = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!")
 else:
-    print("Você errou :(")
+    if(errou_maior):
+        print("Você errou :( .O seu chute foi maior que o numero secreto.")
+    elif(errou_menor ):
+        print("Você errou :( .O seu chute foi menor que o numero secreto.")
 
 print("Fim de jogo")
