b7385762b99d260ac1deb4b6f61c0450a98c5ffe
AnaHeline <anaheline@gmail.com>
2023-02-21 18:54:12 -0300

    Revert "maior e menor"
    This reverts commit 6938f5c0be2891ff233c791978df83f2ee51dda8.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Bem vindo no jogo de adivinhação")
+print("Bem bindo no jogo de adivinhação")
 print("******************************")
 
 numero_secreto=42
@@ -7,17 +7,10 @@ chute_str = input("Digite o seu numero: ")
 
 print("Você digitou", chute_str)
 chute = int(chute_str)
-
-acertou     = numero_secreto == chute
-errou_maior = chute > numero_secreto
-errou_menor = chute < numero_secreto
-
-if (acertou):
+42
+if (numero_secreto == chute):
     print("Você acertou!")
 else:
-    if(errou_maior):
-        print("Você errou :( .O seu chute foi maior que o numero secreto.")
-    elif(errou_menor ):
-        print("Você errou :( .O seu chute foi menor que o numero secreto.")
+    print("Você errou :(")
 
 print("Fim de jogo")
